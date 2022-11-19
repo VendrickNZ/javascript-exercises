68a5bfa949fbb55a995bf9d8fc18d8b1aafb6698
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-19 14:56:15 +1300

    finished q11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let bookArray = [];
+    for (let i = 0; i < array.length; i++) {
+        bookArray.push(array[i].title)
+    }
 
+    return bookArray;
 };
 
 // Do not edit below this line
