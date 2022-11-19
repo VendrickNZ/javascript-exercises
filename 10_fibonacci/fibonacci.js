e0102a864b327de1013749f6fce1de88cc3c17e9
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-19 14:51:46 +1300

    finished q10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) {
+        return "OOPS"
+    }
+    
+    if (n <= 2) {
+        return 1;
+    }
+
+    let firstN = 1;
+    let secondN = 1;
+    let currentN = 0;
+
+    for (let i = 2; i < n; i++) {
+        currentN = firstN + secondN;
+        firstN = secondN;
+        secondN = currentN;
+    }
+
+    return currentN;
 };
 
 // Do not edit below this line
