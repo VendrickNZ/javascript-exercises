cfdaa3bbc417e331a79ad794bbc695ebfb9ee040
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-19 13:52:14 +1300

    finished q8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+    let sum = 0;
+	for (let i = 0; i < numberArray.length; i++) {
+        sum += numberArray[i]
+    }
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(numberArray) {
+    let total = numberArray[0];
+    for (let i = 1; i < numberArray.length; i++) {
+        total *= numberArray[i]
+    }
 
+    return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    if (x == 0) {
+        return 1;
+    }
+	let total = 0;
+    for (let i = 1; i <= x; i++) {
+        total == 0 ? total += i : total *= i;
+    }
+    return total;
 };
 
 // Do not edit below this line
